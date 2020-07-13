6737ffff29c3805d01951e8eb69c81f88c1fcd86
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-12 22:09:15 -0700

    Fixing bug: Disabling scores on click

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -4,7 +4,7 @@ import './RuleRow.css'
 class RuleRow extends Component {
   render() {
     return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
+      <tr className={this.props.score ? "RuleRow RuleRow-disabled" : "RuleRow RuleRow-active"} onClick={this.props.doScore}>
         <td className="RuleRow-name">{this.props.name}</td>
         <td className="RuleRow-score">{this.props.score}</td>
       </tr>
