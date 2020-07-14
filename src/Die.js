874f3e8c0f1f10ca7f658340ede19d5edca2f416
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-13 18:31:31 -0700

    Trying to add css to dice

--- src/Die.js
+++ src/Die.js
@@ -3,9 +3,10 @@ import "./Die.css";
 
 class Die extends Component {
   render() {
+    const classArray = [null, "one", "two", "three", "four", "five", "six"];
     return (
       <button
-        className={"Die"}
+        className={this.props.val ? `Die fas fa-dice-${classArray[this.props.val]}` : `Die`}
         style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
         onClick={() => {this.props.handleClick(this.props.idx)}}
       >
