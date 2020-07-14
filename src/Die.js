a76538b2d8884df64b265533b68dcd4b4560bdad
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-13 18:44:27 -0700

    Refactoring die HandleClick

--- src/Die.js
+++ src/Die.js
@@ -2,13 +2,22 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+  constructor(props){
+    super(props)
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+    this.props.handleClick(this.props.idx)
+  }
+
   render() {
     const classArray = [null, "one", "two", "three", "four", "five", "six"];
     return (
       <button
         className={this.props.val ? `Die fas fa-dice-${classArray[this.props.val]}` : `Die`}
         style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={() => {this.props.handleClick(this.props.idx)}}
+        onClick={this.handleClick}
       >
         {this.props.val}
       </button>
