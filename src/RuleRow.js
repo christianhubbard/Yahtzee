9ea2307a25bce48ae37f74e81b3492f94532543f
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-13 19:21:14 -0700

    Refactoring rulerow class name application

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -4,7 +4,7 @@ import './RuleRow.css'
 class RuleRow extends Component {
   render() {
     return (
-      <tr className={this.props.score ? "RuleRow RuleRow-disabled" : "RuleRow RuleRow-active"} onClick={this.props.score ? null : this.props.doScore}>
+      <tr className={`RuleRow RuleRow-${this.props.score ? "disabled" : "active"}`} onClick={this.props.score ? null : this.props.doScore}>
         <td className="RuleRow-name">{this.props.name}</td>
         <td className="RuleRow-score">{this.props.score ? this.props.score : this.props.desc}</td>
       </tr>
